d89444cdc07d1b640b4e651c985f34911ff6c213
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2025-06-19 14:56:14 -0400

    updating deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,18 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
+import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
 import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    trailingSlash: 'always',
-    site: 'https://mattwaler.com',
-
-    // Single page, no prefetch needed
-    prefetch: false,
-
-    integrations: [
-        tailwind(),
-        sitemap(),
-        compress({
-            CSS: true,
-            SVG: false,
-            Image: false,
-            HTML: {
-                "html-minifier-terser": {
-                    collapseWhitespace: true,
-                    // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
-                    minifyCSS: true,
-                    minifyJS: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    // removeEmptyElements: true, // It removes sometimes SVGs
-                    removeRedundantAttributes: true
-                },
-            },
-            JavaScript: {
-                'terser': {
-                    compress: {
-                        drop_console: true,
-                        drop_debugger: true,
-                    }
-                }
-            }
-        })
-    ]
+  site: 'https://mattwaler.com',
+  devToolbar: {
+    enabled: false,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: 'static',
+  integrations: [alpinejs({ entrypoint: '/src/alpine.config.ts' }), sitemap()],
 });
